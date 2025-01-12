7dc0dfca48c7b0f480fdfd810565acb8ac136a9a
Robert Kuo <r122164152@gmail.com>
2025-01-12 15:34:27 +0800

    update MaxBytesMiddleware

--- middlewares.go
+++ middlewares.go
@@ -4,7 +4,10 @@ import "net/http"
 
 func MaxBytesMiddleware(n int64) HandlerFunc {
 	return func(c *Context) Response {
-		c.Req.body = newRequestBody(http.MaxBytesReader(c.w, c.Req.body, n))
+		w := GetResponseWriter(c)
+		maxBytesBody := http.MaxBytesReader(w, c.Req.req.Body, n)
+		c.Req.req.Body = maxBytesBody
+		c.Req.body = newRequestBody(maxBytesBody)
 		return c.Next()
 	}
 }
