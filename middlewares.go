794a22bd3e79b4b12c0f10387a865c5a8a1bf79d
Robert Kuo <r122164152@gmail.com>
2025-01-13 16:51:46 +0800

    add SetHttpRequest

--- middlewares.go
+++ middlewares.go
@@ -5,8 +5,11 @@ import "net/http"
 func MaxBytesMiddleware(n int64) HandlerFunc {
 	return func(c *Context) Response {
 		w := GetResponseWriter(c)
-		maxBytesBody := http.MaxBytesReader(w, c.Req.req.Body, n)
-		c.Req.req.Body = maxBytesBody
+		httpReq := c.Req.GetHttpRequest()
+
+		maxBytesBody := http.MaxBytesReader(w, httpReq.Body, n)
+
+		httpReq.Body = maxBytesBody
 		c.Req.body = newRequestBody(maxBytesBody)
 		return c.Next()
 	}
